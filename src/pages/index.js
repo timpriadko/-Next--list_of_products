6fde1c8ca235884974a1c43cde23438b938664ac
timpriadko <timpriadko@gmail.com>
2022-07-08 17:08:25 +0300

    fixed header duplicate

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,8 @@ import Layout from "../components/Layout/Layout";
 
 export default function Home() {
   return (
-    <Layout>
-      <div className={styles.container}>
-        <h2>Home Page</h2>
-      </div>
-    </Layout>
+    <div className={styles.container}>
+      <h2>Home Page</h2>
+    </div>
   );
 }
